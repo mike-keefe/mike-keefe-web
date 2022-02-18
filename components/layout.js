467ba5836da832d284b542e1caec81d19b090843
Mike Keefe <67614058+mike-keefe@users.noreply.github.com>
2022-02-18 00:56:52 +0000

    Update og:image

--- components/layout.js
+++ components/layout.js
@@ -18,9 +18,7 @@ export default function Layout({ children, home }) {
         />
         <meta
           property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content={`https://og-image.vercel.app/**Mike%20Keefe**%2C%20Web%20Developer.jpeg?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-white.svg&images=https%3A%2F%2Fwww.mikekeefe.com%2F_next%2Fimage%3Furl%3D%252Fimages%252Fprofile.jpg%26w%3D256%26q%3D75`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
