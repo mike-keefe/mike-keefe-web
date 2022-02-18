2e9493ae66ac027cffe5bf5c152be4999ef973d8
Mike Keefe <67614058+mike-keefe@users.noreply.github.com>
2022-02-18 00:30:22 +0000

    Add google fonts, change colours

--- components/layout.js
+++ components/layout.js
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Mike Keefe is a freelance web designer, developer and runner from London."
         />
         <meta
           property="og:image"
