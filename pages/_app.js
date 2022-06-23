c26db7fee4355a0cdf80efd6fcd1e56892991fcf
Mike Keefe <67614058+mike-keefe@users.noreply.github.com>
2022-06-23 21:40:59 +0100

    Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
 import '../styles/global.css'
 
 export default function App({ Component, pageProps }) {
+    const router = useRouter()
+
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+                page_path: url,
+            })
+        }
+        router.events.on('routeChangeComplete', handleRouteChange)
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange)
+        }
+    }, [router.events]
+    )
     return <Component {...pageProps} />
 }
--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,22 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
           <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100;0,400;0,700;1,100&display=swap" rel="stylesheet" />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
